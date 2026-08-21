2a7f34e2bd49ae56d01391d0196aadf5a87deea8
t4sn3m <tasneemnsour27@gmail.com>
2026-08-21 22:06:24 +0300

    Initial commit - Watermelon solved

---
+++ 4A_Watermelon.cpp
@@ -0,0 +1,20 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    if(w>2){
+    if(w%2==0){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+    }
+    else{
+        cout<<"No\n";
+    }
+    return 0;
+}
